40a7fae64ad6618ddc4a78108ab6c886845720f5
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-24 15:28:49 +0800

    [ParkingLot][mandy chen] test : Add should_return_ticket_when_park_given_car test

---
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -0,0 +1,15 @@
+package com.oocl.cultivation;
+
+
+import sun.security.krb5.internal.Ticket;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLot {
+//    private Map<CarTicket,Car> parkingRooms=new HashMap<>();
+    public CarTicket park(Car car) {
+//        return new CarTicket();
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -0,0 +1,21 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_ticket_when_park_given_car() {
+        //given
+        ParkingLot parkingLot=new ParkingLot();
+        Car car=new Car();
+        //when
+        CarTicket ticket=parkingLot.park(car);
+        //then
+        assertNotNull(ticket);
+    }
+}
